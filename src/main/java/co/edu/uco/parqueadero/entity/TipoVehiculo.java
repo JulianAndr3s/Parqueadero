ac92f11d4b5163e7a204d4b3292f67c8d73978ba
Julián Botero <juanbo97@hotmail.com>
2019-11-16 15:15:19 -0500

    Creación de la Entidad
    Se implementa la clase Entity.

--- src/main/java/co/edu/uco/parqueadero/entity/TipoVehiculo.java
+++ src/main/java/co/edu/uco/parqueadero/entity/TipoVehiculo.java
@@ -1,5 +1,21 @@
 package co.edu.uco.parqueadero.entity;
 
+
+import javax.persistence.*;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "tipo_vehiculo")
 public class TipoVehiculo {
+	
+	@Id
+	@Column(name="id")
+	private Long id;	
+	@Column(name="tipo")
+	private String tipo;	
+	@Column(name="cilindraje")
+	private String cilindraje;
 
 }
--- src/main/java/co/edu/uco/parqueadero/entity/Vehiculo.java
+++ src/main/java/co/edu/uco/parqueadero/entity/Vehiculo.java
@@ -1,5 +1,35 @@
 package co.edu.uco.parqueadero.entity;
 
+import javax.persistence.*;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "vehiculo")
 public class Vehiculo {
+	
+	@Id
+	@GeneratedValue
+	@Column(name="id")
+	private Long id;
+	@Column(name="propietario")
+	private String propietario;
+	@Column(name="placa")
+	private String placa;
+	@Column(name="telefono")
+	private String telefono;
+	@Column(name="estado")
+	private String estado;
+	@ManyToOne
+    @JoinColumn(name="id_tipo_vehiculo")
+	private TipoVehiculo tipoVehiculo;
+	
+	public TipoVehiculo getTipoVehiculo() {
+		if (tipoVehiculo == null) {
+			tipoVehiculo = new TipoVehiculo();
+		}
+		return tipoVehiculo;
+	}
 
 }
