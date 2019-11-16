64f117c6f680abb0362f65c5388e0c40971a8f4b
Julián Botero <juanbo97@hotmail.com>
2019-11-16 16:27:41 -0500

    Creación Import SQL

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.tipo_vehiculo(id, tipo, cilindraje) VALUES (1, 'Carro', 'Cualquiera');
+INSERT INTO public.tipo_vehiculo(id, tipo, cilindraje) VALUES (2, 'Moto', 'Alto');
+INSERT INTO public.tipo_vehiculo(id, tipo, cilindraje) VALUES (3, 'Moto', 'Bajo');
